4753f7f9934d5cb495973c3db53c223584aedfad
Vikashweps <vikascshegoleva@gmail.com>
2025-10-22 13:27:50 +0700

    first commit

--- 2/build/convert_mp3.py
+++ 2/build/convert_mp3.py
@@ -3,6 +3,7 @@ import librosa
 from pydub import AudioSegment
 from pydub.playback import play
 import os
+import matplotlib.pyplot as plt
 
 mp3_file = "Smeshariki.mp3"  # исходный файл
 pcm_file = "tx.pcm" 
@@ -35,3 +36,31 @@ audio = AudioSegment(
 audio.export(output_mp3, format="mp3", bitrate="192k")
 print(f"PCM файл '{pcm_file2}' успешно преобразован обратно в MP3: '{output_mp3}'")
 
+
+# Чтение файла .pcm
+name = "rx.pcm"
+
+real = []
+imag = []
+count = []
+counter = 0
+
+with open(name, "rb") as f:
+    index = 0
+    while (byte := f.read(2)):
+        if index % 2 == 0:
+            real.append(int.from_bytes(byte, byteorder='little', signed=True))
+            count.append(counter)
+            counter += 1
+        else:
+            imag.append(int.from_bytes(byte, byteorder='little', signed=True))
+        index += 1
+
+# Построение графика
+plt.plot(count, real, color='blue', label='I')
+plt.plot(count, imag, color='red', label='Q')
+plt.xlabel('Sample Index')
+plt.ylabel('Amplitude')
+plt.legend()
+plt.title('RX Signal I/Q Components')
+plt.show()
